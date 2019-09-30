e784e0bfa7806eb73929516c07da30295f0d67b0
Fabrice Demarthon <fabricedemarthon@neo116.neo.ehess.fr>
2019-09-30 15:19:36 +0200

    node env

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const next = require("next");
 const elasticAPI = require("./elastic-api");
 
-const app = next();
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const cors = require("cors");
