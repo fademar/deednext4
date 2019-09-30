8f3ab1657099fa2ff393230d6cce3851efba0aa2
Fabrice Demarthon <fabricedemarthon@neo116.neo.ehess.fr>
2019-09-30 19:00:30 +0200

    url proxy

--- elastic-api.js
+++ elastic-api.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const router = express.Router();
 
 const client = new Client({
-  node: "http://localhost:9200"
+  node: "http://192.168.44.41:8080"
 });
 
 router.use(bodyParser.json());
