04a635dfab9c6b8e1567f544a7ab7709fe6735f5
Fabrice Demarthon <fabricedemarthon@neo116.neo.ehess.fr>
2019-10-01 12:54:26 +0200

    ports

--- components/Layout/Container.js
+++ components/Layout/Container.js
@@ -4,7 +4,7 @@ import { ReactiveBase } from "@appbaseio/reactivesearch";
 function AppLayout(props) {
   return (
     <Layout className="layout" style={{ height: "100vh", overflow: "auto" }}>
-      <ReactiveBase app="deeds" url="http://192.168.44.41:8080/deeds">
+      <ReactiveBase app="deeds" url="http://elastic:8080/">
         {props.children}
       </ReactiveBase>
     </Layout>
