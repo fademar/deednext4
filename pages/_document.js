5c9cd37ff4c8af6de539c58ad2e8c54f9ad213ae
fademar <fdemarthon@gmail.com>
2019-10-02 15:53:36 +0200

    static

--- pages/_document.js
+++ pages/_document.js
@@ -14,11 +14,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            rel="stylesheet/less"
-            type="text/css"
-            href="/_next/static/styles.less"
-          />
           <meta charSet="utf-8" />
           <meta
             name="viewport"
