be8dbd86eec2f5825426c918f77e2066bc2d0a91
fademar <fdemarthon@gmail.com>
2019-10-02 16:20:54 +0200

    nextdir

--- next.config.js
+++ next.config.js
@@ -1,40 +1,41 @@
 /* eslint-disable */
-const withLess = require('@zeit/next-less')
-const lessToJS = require('less-vars-to-js')
-const fs = require('fs')
-const path = require('path')
+const withLess = require("@zeit/next-less");
+const lessToJS = require("less-vars-to-js");
+const fs = require("fs");
+const path = require("path");
 
 // Where your antd-custom.less file lives
 const themeVariables = lessToJS(
-  fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
-)
+  fs.readFileSync(path.resolve(__dirname, "./assets/antd-custom.less"), "utf8")
+);
 
 module.exports = withLess({
   lessLoaderOptions: {
     javascriptEnabled: true,
-    modifyVars: themeVariables, // make your antd custom effective
+    modifyVars: themeVariables // make your antd custom effective
   },
+  distDir: "_next",
   webpack: (config, { isServer }) => {
     if (isServer) {
-      const antStyles = /antd\/.*?\/style.*?/
-      const origExternals = [...config.externals]
+      const antStyles = /antd\/.*?\/style.*?/;
+      const origExternals = [...config.externals];
       config.externals = [
         (context, request, callback) => {
-          if (request.match(antStyles)) return callback()
-          if (typeof origExternals[0] === 'function') {
-            origExternals[0](context, request, callback)
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === "function") {
+            origExternals[0](context, request, callback);
           } else {
-            callback()
+            callback();
           }
         },
-        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-      ]
+        ...(typeof origExternals[0] === "function" ? [] : origExternals)
+      ];
 
       config.module.rules.unshift({
         test: antStyles,
-        use: 'null-loader',
-      })
+        use: "null-loader"
+      });
     }
-    return config
-  },
-})
+    return config;
+  }
+});
