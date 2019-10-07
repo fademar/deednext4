f27b548f0a9fb2c007a3cf8045aa29d263097db2
Fabrice Demarthon <fabricedemarthon@neo116.neo.ehess.fr>
2019-10-07 17:38:36 +0200

    facebot

--- components/Data/Facetbox.js
+++ components/Data/Facetbox.js
@@ -1,8 +1,10 @@
-import { Tree, Input } from "antd";
+import { Select } from "antd";
 import * as _ from "lodash";
 
 const FacetBox = props => {
   console.log(props.textFields);
+
+  return <></>;
 };
 
 export default FacetBox;
