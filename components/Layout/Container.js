ef2994ea05ed30a8f99f6633ce453c86d1e25961
Fabrice Demarthon <fabricedemarthon@neo116.neo.ehess.fr>
2019-10-07 12:26:30 +0200

    host

--- components/Layout/Container.js
+++ components/Layout/Container.js
@@ -4,7 +4,7 @@ import { ReactiveBase } from "@appbaseio/reactivesearch";
 function AppLayout(props) {
   return (
     <Layout className="layout" style={{ height: "100vh", overflow: "auto" }}>
-      <ReactiveBase app="deeds" url="http://localhost:9200">
+      <ReactiveBase app="deeds" url="http://neo041:8080">
         {props.children}
       </ReactiveBase>
     </Layout>
